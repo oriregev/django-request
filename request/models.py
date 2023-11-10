6496b1bd0c3193bad0ac4aa5734aa0ee5d0386a4
Ori Regev <ori.regev@gmail.com>
2023-11-10 17:16:45 +0200

    Fix validation

--- request/models.py
+++ request/models.py
@@ -76,7 +76,7 @@ class Request(models.Model):
             ]
         self.ip = next((item for item in ip_addr if item is not None), None)
         from django.core.validators import validate_ipv46_address
-        if self.ip is None or self.ip == '' or self.ip is not is_valid_ip(self.ip):
+        if self.ip is None or self.ip == '' or self.ip is not self.is_valid_ip():
             self.ip = request_settings.IP_DUMMY
         # self.ip = request.META.get('REMOTE_ADDR', '')
 
